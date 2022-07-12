376b0bb737920a25f7b22bd9e6a3338f3531f673
erikringwalters <erik.ring.walters@gmail.com>
2022-07-11 21:52:10 -0700

    initial commit

---
+++ blink.py
@@ -0,0 +1,12 @@
+from machine import Pin
+from time import sleep
+
+led = machine.Pin("LED", machine.Pin.OUT)
+
+n = 0
+
+while True:
+    led.toggle()
+    print("13 times {} is {}".format(n, 13*n))
+    n += 1
+    sleep(0.05)
