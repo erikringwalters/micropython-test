87a61ec7d777a7d40fda394caf4fba5195d09033
erikringwalters <erik.ring.walters@gmail.com>
2022-07-12 23:30:25 -0700

    multiple servos (2 & 3) running

--- blink.py
+++ blink.py
@@ -7,6 +7,6 @@ n = 0
 
 while True:
     led.toggle()
-    print("13 times {} is {}".format(n, 13*n))
-    n += 1
-    sleep(0.25)
+    # print("13 times {} is {}".format(n, 13*n))
+    # n += 1
+    sleep(5)
--- servo2.py
+++ servo2.py
@@ -8,17 +8,27 @@ MAX = 2000000
 led = machine.Pin("LED", machine.Pin.OUT)
 
 pwm15 = PWM(Pin(15))
+pwm12 = PWM(Pin(12))
 
 pwm15.freq(50)
 pwm15.duty_ns(MID)
 
-while True: 
-    print("Starting")
+pwm12.freq(50)
+pwm12.duty_ns(MID)
+
+print("Starting")
+
+while True:     
     led.on()
 
     pwm15.duty_ns(MIN)
+    pwm12.duty_ns(MIN)
     utime.sleep(1)
+
     pwm15.duty_ns(MID)
+    pwm12.duty_ns(MID)
     utime.sleep(1)
+    
     pwm15.duty_ns(MAX)
+    pwm12.duty_ns(MAX)
     utime.sleep(1)
